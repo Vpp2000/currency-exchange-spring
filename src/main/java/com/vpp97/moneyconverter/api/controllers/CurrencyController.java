242387151324016e57d95607dd1d227928372267
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-26 16:40:17 -0500

    Now we can obtain details of coins

--- src/main/java/com/vpp97/moneyconverter/api/controllers/CurrencyController.java
+++ src/main/java/com/vpp97/moneyconverter/api/controllers/CurrencyController.java
@@ -1,14 +1,18 @@
 package com.vpp97.moneyconverter.api.controllers;
 
+import com.vpp97.moneyconverter.api.services.CurrencyService;
 import com.vpp97.moneyconverter.api.services.ExchangeService;
 import com.vpp97.moneyconverter.dto.request.CreateCurrencyExchangeRequest;
 import com.vpp97.moneyconverter.dto.request.UpdateCurrencyExchangeRequest;
+import com.vpp97.moneyconverter.dto.response.CurrencyDetail;
 import com.vpp97.moneyconverter.dto.response.CurrencyExchangeResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,14 +27,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Currency")
 public class CurrencyController {
     private final ExchangeService exchangeService;
-    @PostMapping("rate")
+    private final CurrencyService currencyService;
+
+    @GetMapping(value = "{currencyId}", produces = "application/json")
+    public ResponseEntity<CurrencyDetail> getCurrencyDetail(@PathVariable("currencyId") Long currencyId){
+        CurrencyDetail currencyDetail = this.currencyService.getCurrencyDetailById(currencyId);
+        return ResponseEntity.ok(currencyDetail);
+    }
+
+    @PostMapping(value = "rate", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<CurrencyExchangeResponse> createCurrencyExchange(@RequestBody CreateCurrencyExchangeRequest createCurrencyExchangeRequest){
         CurrencyExchangeResponse currencyExchangeResponse = this.exchangeService.createCurrencyExchange(createCurrencyExchangeRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(currencyExchangeResponse);
     }
 
-    @PutMapping("{currencyId}/rate")
+    @PutMapping(value = "{currencyId}/rate", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<CurrencyExchangeResponse> updateCurrencyExchange(@PathVariable("currencyId") Long currencyId, @RequestBody UpdateCurrencyExchangeRequest updateCurrencyExchangeRequest){
         CurrencyExchangeResponse currencyExchangeResponse = this.exchangeService.updateCurrencyExchange(currencyId, updateCurrencyExchangeRequest);
